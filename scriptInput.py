8710b4cdea5de04558715c24959d97425716816d
DreamchaserFF <118946745+DreamchaserFF@users.noreply.github.com>
2024-10-08 17:05:39 -0300

    Add files via upload

---
+++ scriptInput.py
@@ -0,0 +1,10 @@
+nome = input ("Qual é o seu nome? ")
+print ("Olá,",nome,", muito prazer!")
+dia = input ("Qual é o dia do seu nascimento? ")
+mes = input ("Qual é o mês do seu nascimento? ")
+ano = input ("Qual é o ano do seu nascimento? ")
+print ("Você nasceu no dia",dia, "de",mes, "de",ano,",correto?")
+num1 = input ("Informe o primeiro número: ")
+num2 = input ("Informe o segundo número: ")
+soma = num1 + num2
+print ("A soma entre os dois número é:",soma)
